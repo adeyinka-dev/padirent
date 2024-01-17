791cc2c0ffd75399608d167d22e9ba27beb9bc6e
synxpay <admin@synxpay.com>
2024-01-17 21:29:48 +0000

    ready

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-xy8$my=wufzdm-q5vcs!90%#yb*nmhb5n995r!o5qpqzo_aj^o"
+SECRET_KEY = env.str("DJANGO_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "whitenoise.runserver_nostatic",
     "django.contrib.staticfiles",
     # #3rd-Party
     "cloudinary_storage",
@@ -71,6 +72,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -80,12 +82,7 @@ MIDDLEWARE = [
     "allauth.account.middleware.AccountMiddleware",
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "http://127.0.0.1:9000",
-    "http://localhost:8080",
-    "exp://192.168.134.146:8081",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 AUTH_USER_MODEL = "accounts.CustomUser"
@@ -130,12 +127,17 @@ CLOUDINARY_STORAGE = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
+import dj_database_url
+
+DATABASES = {"default": dj_database_url.parse(env.str("DB_URL"))}
 
 
 # Password validation
@@ -173,6 +175,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]  # new
+STATIC_ROOT = BASE_DIR / "staticfiles"  # new
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
 MEDIA_URL = "/media/"
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
--- listings/views.py
+++ listings/views.py
@@ -1,21 +1,23 @@
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
 
 from .models import Listing
 from .permissions import IsAgentOrReadOnly
 from .serializers import ListingSerializer, UserSerializer
 
 
-# class ListingsView(generics.ListAPIView):
-#     queryset = Listing.objects.all()
-#     serializer_class = ListingSerializer
-
-
 class ListingViewSet(viewsets.ModelViewSet):
-    permission_classes = (IsAgentOrReadOnly,)
     queryset = Listing.objects.all()
     serializer_class = ListingSerializer
+
+    # If user is not authenticated, remove modification from view
+    def get_permissions(self):
+        if self.action in ["list", "retrieve"]:
+            permission_classes = [IsAuthenticatedOrReadOnly]
+        else:
+            permission_classes = [IsAgentOrReadOnly]
+        return [permission() for permission in permission_classes]
 
 
 class AgentViewSet(viewsets.ModelViewSet):
